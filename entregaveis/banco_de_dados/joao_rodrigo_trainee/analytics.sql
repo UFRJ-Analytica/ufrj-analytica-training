5cbd3fe00b9bbd1cdc4a6ec4de0a46d138a83ad7
joaordc <joaoroddias@gmail.com>
2026-07-20 13:57:45 -0300

    feat: segunda parte, queries e analytics finalizados

--- entregaveis/banco_de_dados/joao_rodrigo_trainee/analytics.sql
+++ entregaveis/banco_de_dados/joao_rodrigo_trainee/analytics.sql
@@ -1,3 +1,94 @@
 -- analytics.sql
 -- Escreva aqui consultas analíticas ou uma proposta de camada analítica.
 -- Exemplos: rankings, agregações por região/UF, views, tabela fato/dimensões.
+
+-- Vou responder uma pergunta de cada eixo descrito na entrega:
+-- Comparação, ranking, distribuição, classificação e síntese dos dados.
+
+------------------------------------------------------------------------------------------
+
+-- Comparação: Quais municípios possuem população acima da média nacional dos municípios?
+-- 
+-- Aqui foi necessário apenas um join, com a tabela de população municipal.
+
+SELECT 
+    m.nome_municipio, 
+    pm.valor 
+FROM municipios m
+JOIN populacao_municipal pm ON m.id_municipio = pm.id_municipio 
+WHERE pm.valor > (
+    SELECT AVG(pm2.valor)
+    FROM populacao_municipal pm2
+);
+
+-- Ranking: Quais são os municípios mais populosos de uma UF escolhida?
+-- 
+-- Escolhi fazer o top 10 do estado do Rio de Janeiro
+
+SELECT m.nome_municipio, pm.valor
+FROM municipios m
+JOIN populacao_municipal pm ON m.id_municipio = pm.id_municipio 
+JOIN estados e ON m.id_uf = e.id_uf 
+WHERE e.nome_uf = 'Rio de Janeiro'
+ORDER BY pm.valor DESC
+LIMIT 10;
+
+-- Distribuição: Quais estados possuem maior concentração populacional em poucos municípios?
+--
+-- Nesse caso, abordei o problema somando a população dos 5 municípios mais populosos de 
+-- 		cada estado e, em seguida, dividindo pela população total daquele estado.
+-- O Distrito Federal não possui municípios, então foi desconsiderado.
+-- Como o enunciado não pediu um ranking, limitei a resposta apenas aos 5 estados com maior 
+-- 		índice.
+
+SELECT 
+	e.nome_uf,
+	(
+        SELECT SUM(valor) 
+        FROM (
+            SELECT pm2.valor 
+            FROM populacao_municipal pm2 
+            JOIN municipios m2 ON pm2.id_municipio = m2.id_municipio 
+            WHERE m2.id_uf = e.id_uf 
+            ORDER BY pm2.valor DESC 
+            LIMIT 5
+        )
+    ) / SUM(pm.valor) AS indice
+FROM estados e
+JOIN municipios m ON e.id_uf = m.id_uf 
+JOIN populacao_municipal pm ON m.id_municipio = pm.id_municipio 
+WHERE e.nome_uf != 'Distrito Federal'
+GROUP BY e.id_uf, e.nome_uf 
+ORDER BY indice DESC
+LIMIT 5;
+
+-- Classificação: Quantos municípios pequenos, médios e grandes existem na base?
+--
+-- Classifiquei de forma arbitrária, com municípios pequenos sendo os que possuem população 
+-- 		menor que 100 mil, os médios, entre 100 mil e 500 mil (inclusos), e os grandes, acima de 
+-- 		500 mil.
+
+SELECT 
+	CASE 
+		WHEN pm.valor < 100000 THEN 'Pequeno'
+		WHEN pm.valor BETWEEN 100000 AND 500000 THEN 'Médio'
+		ELSE 'Grande'
+	END AS tamanho, 
+	COUNT(m.id_municipio) AS quantidade
+FROM municipios m 
+JOIN populacao_municipal pm ON m.id_municipio = pm.id_municipio
+GROUP BY tamanho
+ORDER BY quantidade DESC;
+
+-- Síntese dos dados: Qual é a população média dos municípios por estado?
+-- 
+-- Aqui, eu agreguei por estado e calculei a média, com join nas tabelas
+-- 		de municípios e população municipal
+
+SELECT 
+	e.nome_uf, 
+	AVG(pm.valor) AS media_populacional
+FROM estados e
+JOIN municipios m ON e.id_uf = m.id_uf 
+JOIN populacao_municipal pm ON m.id_municipio = pm.id_municipio 
+GROUP BY e.id_uf, e.nome_uf;
--- entregaveis/banco_de_dados/joao_rodrigo_trainee/queries.sql
+++ entregaveis/banco_de_dados/joao_rodrigo_trainee/queries.sql
@@ -1,3 +1,129 @@
 -- queries.sql
 -- Escreva aqui suas consultas SQL obrigatórias.
 -- Inclua SELECT, JOIN, GROUP BY, INSERT, UPDATE e DELETE.
+
+-- 1. Quais regiões existem na base?
+
+SELECT nome_regiao 
+FROM regioes;
+
+-- 2. Quais estados pertencem a uma região escolhida?!
+-- Escolhendo a região Sudeste:
+
+SELECT nome_uf 
+FROM estados e 
+INNER JOIN regioes r ON e.id_regiao = r.id_regiao 
+WHERE r.nome_regiao = 'Sudeste';
+
+-- 3. Quais municípios pertencem a uma UF escolhida?
+-- Escolhendo o Rio de Janeiro:
+
+SELECT nome_municipio
+FROM municipios m 
+INNER JOIN estados e ON m.id_uf = e.id_uf 
+WHERE e.nome_uf = 'Rio de Janeiro';
+
+-- 4. Qual é o estado e a região de cada município?
+
+SELECT 
+	m.nome_municipio, 
+	e.nome_uf, 
+	r.nome_regiao 
+FROM municipios m
+INNER JOIN estados e ON m.id_uf = e.id_uf 
+INNER JOIN regioes r ON e.id_regiao = r.id_regiao;
+
+-- 5. Qual é a população estimada dos municípios, mostrando município, UF, região, ano e valor?
+
+SELECT 
+	m.nome_municipio, 
+	e.nome_uf, 
+	r.nome_regiao, 
+	pm.ano, 
+	pm.valor AS "População estimada"
+FROM municipios m
+INNER JOIN estados e ON m.id_uf = e.id_uf 
+INNER JOIN regioes r ON e.id_regiao = r.id_regiao 
+INNER JOIN populacao_municipal pm ON m.id_municipio = pm.id_municipio;
+
+-- 6. Quantos municípios existem por estado?
+
+SELECT 
+	e.nome_uf, 
+	COUNT(m.id_municipio) AS "Quantidade de municípios"
+FROM estados e
+INNER JOIN municipios m ON e.id_uf = m.id_uf
+GROUP BY e.nome_uf;
+
+-- 7. Quantos municípios existem por região?
+
+SELECT 
+	r.nome_regiao, 
+	COUNT(m.id_municipio) AS "Quantidade de municípios"
+FROM regioes r
+INNER JOIN estados e ON r.id_regiao = e.id_regiao 
+INNER JOIN municipios m ON e.id_uf = m.id_uf
+GROUP BY r.nome_regiao;
+
+-- 8. Qual é a população total estimada por estado?
+
+SELECT 
+	e.nome_uf, 
+	SUM(pm.valor) AS "População estimada por estado"
+FROM estados e 
+INNER JOIN municipios m ON e.id_uf = m.id_uf
+INNER JOIN populacao_municipal pm ON m.id_municipio = pm.id_municipio 
+GROUP BY e.nome_uf;
+
+-- 9. Quais estados possuem uma quantidade elevada de municípios (Top 10 Estados)?
+
+SELECT 
+	e.nome_uf, 
+	COUNT(m.id_municipio) AS "Quantidade de municípios"
+FROM estados e
+INNER JOIN municipios m ON e.id_uf = m.id_uf 
+GROUP BY e.nome_uf
+ORDER BY COUNT(m.id_municipio) DESC 
+LIMIT 10;
+
+-- 10. Crie uma tabela simples de teste e registre uma inserção, uma alteração e uma remoção de registro
+-- nessa tabela.
+
+-- Query para fins de teste:
+DROP TABLE IF EXISTS populacao_estadual;
+
+-- Vou fazer uma tabela baseada no item 8, sobre a população de cada estado
+CREATE TABLE populacao_estadual (
+	id_uf INTEGER PRIMARY KEY,
+	nome_uf TEXT NOT NULL,
+	ano INTEGER NOT NULL,
+	valor FLOAT NOT NULL,
+	unidade TEXT NOT NULL,
+	fonte TEXT NOT NULL
+);
+
+-- Populando a tabela baseada na query do item 8
+INSERT INTO populacao_estadual (id_uf, nome_uf, ano, valor, unidade, fonte)
+SELECT 
+	e.id_uf,
+	e.nome_uf,
+	pm.ano,
+	SUM(pm.valor),
+	pm.unidade,
+	pm.fonte
+FROM estados e 
+INNER JOIN municipios m ON e.id_uf = m.id_uf
+INNER JOIN populacao_municipal pm ON m.id_municipio = pm.id_municipio 
+GROUP BY e.id_uf, e.nome_uf, pm.ano, pm.unidade, pm.fonte;
+
+-- Alterando a população do Rio de Janeiro 
+UPDATE populacao_estadual
+SET valor = valor + 10000
+WHERE nome_uf = 'Rio de Janeiro';
+
+-- Query para conferir a alteração:
+SELECT valor FROM populacao_estadual pe WHERE pe.nome_uf = 'Rio de Janeiro';
+
+-- Removendo a linha do Rio de Janeiro
+DELETE FROM populacao_estadual 
+WHERE nome_uf = 'Rio de Janeiro';
