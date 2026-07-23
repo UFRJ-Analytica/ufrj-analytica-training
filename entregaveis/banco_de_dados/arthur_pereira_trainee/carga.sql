cecf19e655ef63a7e84291efe4e99c5a0e0b2777
Arthur Pereira <arthur.montpereira@gmail.com>
2026-07-23 00:32:05 -0300

    finaliza entrega de banco de dados I

--- entregaveis/banco_de_dados/arthur_pereira_trainee/carga.sql
+++ entregaveis/banco_de_dados/arthur_pereira_trainee/carga.sql
@@ -1,2 +1,50 @@
 -- carga.sql
 -- Defina aqui as instruções para carregar os dados das tabelas brutas para as tabelas normalizadas.
+-- carga.sql
+PRAGMA foreign_keys = ON;
+INSERT INTO regioes (
+id_regiao,
+sigla_regiao,
+nome_regiao
+)
+SELECT DISTINCT
+id_regiao,
+sigla_regiao,
+nome_regiao
+FROM raw_regioes
+WHERE id_regiao IS NOT NULL;
+
+insert into estados (
+id_uf,
+sigla_uf,
+nome_uf,
+id_regiao)
+select distinct 
+id_uf,
+sigla_uf,
+nome_uf,
+id_regiao
+from raw_estados 
+where id_uf is not null;
+
+insert into municipios (
+id_municipio,
+nome_municipio,
+id_uf)
+select distinct
+id_municipio,
+nome_municipio,
+id_uf
+from raw_municipios 
+where id_municipio is not null;
+
+insert into populacao_municipal (
+id_municipio,
+ano,
+valor)
+select distinct
+id_municipio,
+ano,
+valor
+from raw_populacao_municipal 
+where id_municipio is not null and ano is not null;
--- entregaveis/banco_de_dados/arthur_pereira_trainee/schema.sql
+++ entregaveis/banco_de_dados/arthur_pereira_trainee/schema.sql
@@ -1,2 +1,38 @@
 -- schema.sql
 -- Defina aqui a estrutura do banco de dados normalizado.
+-- schema.sql
+PRAGMA foreign_keys = ON;
+DROP TABLE IF EXISTS populacao_municipal;
+DROP TABLE IF EXISTS municipios;
+DROP TABLE IF EXISTS estados;
+DROP TABLE IF EXISTS regioes;
+
+CREATE TABLE regioes (
+id_regiao INTEGER PRIMARY KEY,
+sigla_regiao TEXT NOT NULL,
+nome_regiao TEXT NOT NULL
+);
+
+create table estados (
+id_uf integer primary key, 
+sigla_uf text not null,
+nome_uf text not null,
+id_regiao integer not null,
+foreign key (id_regiao) references regioes (id_regiao)
+);
+
+create table municipios (
+id_municipio integer primary key,
+nome_municipio text not null,
+id_uf integer not null,
+foreign key (id_uf) references estados (id_uf)
+);
+
+create table populacao_municipal (
+id_municipio integer not null,
+ano integer not null,
+valor integer not null,
+primary key (id_municipio,ano),
+foreign key (id_municipio) references municipios(id_municipio)
+);
+
