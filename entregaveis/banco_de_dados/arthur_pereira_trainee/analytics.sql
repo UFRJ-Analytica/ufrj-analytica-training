a6d46d74b37a976e526f9e7f95d2a61fa459d384
Arthur Pereira <arthur.montpereira@gmail.com>
2026-07-23 00:26:53 -0300

    arquivos faltantes

--- entregaveis/banco_de_dados/arthur_pereira_trainee/analytics.sql
+++ entregaveis/banco_de_dados/arthur_pereira_trainee/analytics.sql
@@ -1,3 +1,42 @@
 -- analytics.sql
 -- Escreva aqui consultas analíticas ou uma proposta de camada analítica.
 -- Exemplos: rankings, agregações por região/UF, views, tabela fato/dimensões.
+
+
+--1. Quais são os municípios mais populosos da base?
+select nome_municipio, valor as populacao from municipios natural join populacao_municipal 
+order by 2 desc;
+-- o munícipio mais populoso é São Paulo, com quase 12 milhões de habitantes. Em seguida temos o Rio de Janeiro, com um pouco mais da metade do 1º e em terceiro lugar temos Brasília com apenas 3 milhões.
+
+
+--2. Quais são os municípios mais populosos de uma UF escolhida?
+select nome_municipio, valor as populacao from municipios natural join populacao_municipal natural join estados 
+where sigla_uf="ES"
+order by 2 desc
+limit 5;
+--Curiosamente, a capital de Espírito Santo é apenas o 4º município mais populoso, estando atrás de Serra (1º),Vila Velha (2º) e Cariacica (3º). Ao menos Vitória está à frente de Cachoeiro de Itapemirim (5º).
+
+
+--4. Qual é a população média dos municípios por estado?
+select nome_uf,round(avg(valor)) as populacao_media from municipios natural join populacao_municipal natural join estados 
+group by nome_uf
+order by 2 desc;
+--Distrito Federal, de forma bem destoante, está em primeiro com 3 milhões de habitantes. Em seguida temos o Rio de Janeiro, que apesar de ter uma capital com 6 milhões de 
+--habitantes (como visto antes) possui apenas uma média de  187 mil habitantes/município. E em terceiro temos São Paulo, com uma média de 70 mil habitantes/município.
+
+
+--5. Quais municípios possuem população acima da média nacional dos municípios?
+select nome_municipio, valor as populacao_media from municipios natural join populacao_municipal where valor > (select avg(valor) from populacao_municipal)
+order by 2 desc;
+-- São 902 munícipios que possuem população acima da média nacional dos municípios, com o 1º sendo São Paulo e o último sendo Amarante do Maranhão com 38.335 habitantes. Portanto dá pra deduzir que a média nacional seja de 38 mil.
+
+
+--8. Qual região concentra a maior população estimada?
+select nome_regiao, sum(valor) as populacao_total
+from populacao_municipal natural join municipios natural join estados natural join regioes
+group by nome_regiao
+order by 2 desc;
+--A região com maior população é o Sudeste, com 88 milhões de habitantes. Em seguida temos o Nordeste, com 57 milhões e em terceiro o Sul com 31 milhões. Norte e Centro-Oeste estão em último, com respectivamente 18 e 17 milhões.
+
+
+
--- entregaveis/banco_de_dados/arthur_pereira_trainee/queries.sql
+++ entregaveis/banco_de_dados/arthur_pereira_trainee/queries.sql
@@ -1,3 +1,57 @@
 -- queries.sql
 -- Escreva aqui suas consultas SQL obrigatórias.
 -- Inclua SELECT, JOIN, GROUP BY, INSERT, UPDATE e DELETE.
+
+--1. Quais regiões existem na base?
+select nome_regiao as regiao from regioes
+order by regiao;   
+
+--2. Quais estados pertencem a uma região escolhida?
+select nome_uf as estados from estados join regioes on regioes.id_regiao=estados.id_regiao
+where regioes.nome_regiao = 'Norte';
+
+--3. Quais municípios pertencem a uma UF escolhida?
+select nome_municipio as municipios from municipios join estados on municipios.id_uf=estados.id_uf
+where nome_uf = 'Espírito Santo';
+
+--4. Qual é o estado e a região de cada município?
+select nome_municipio,nome_uf,nome_regiao 
+from municipios join estados on municipios.id_uf=estados.id_uf join regioes on regioes.id_regiao=estados.id_regiao;
+
+--5. Qual é a população estimada dos municípios, mostrando município, UF, região, ano e valor?
+select nome_municipio as municipio,nome_uf as UF,nome_regiao as regiao,ano,valor
+from populacao_municipal natural join municipios natural join estados natural join regioes;
+
+--6. Quantos municípios existem por estado?
+select nome_uf, count(nome_municipio) as quantidade_de_municipios
+from municipios natural join estados
+group by nome_uf
+order by 2 desc; 
+
+--7. Quantos municípios existem por região?
+select nome_regiao, count(nome_municipio) as quantidade_de_municipios
+from municipios natural join estados natural join regioes
+group by nome_regiao
+order by 2 desc; 
+
+--8. Qual é a população total estimada por estado?
+select nome_uf, valor as populacao_total
+from populacao_municipal natural join municipios natural join estados
+group by nome_uf
+order by 2 desc;
+
+--9. Quais estados possuem uma quantidade elevada de municípios (Top 10 Estados)?
+select nome_uf, count(nome_municipio) as quantidade_de_municipios
+from municipios natural join estados
+group by nome_uf
+order by 2 desc
+limit 10;
+
+--10. Crie uma tabela simples de teste e registre uma inserção, uma alteração e uma remoção de registro nessa tabela.
+create table aliens_ben_10 (
+id integer primary key autoincrement,
+nome varchar(100));
+insert into aliens_ben_10 (nome) values ('calafrio');
+update aliens_ben_10 set nome = 'friagem' where id = 1;
+delete from aliens_ben_10 where id = 1;
+drop table aliens_ben_10;
